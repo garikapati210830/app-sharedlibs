c24451866ccf10e9eb3f58d74a9868169c3b966b
garikapati210830 <100381872+garikapati210830@users.noreply.github.com>
2022-03-21 13:07:49 +0800

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -4,6 +4,11 @@ def call(ip,user,credId){
      sh "scp -o StrictHostKeyChecking=no target/*.war ${user}@${ip}:/opt/tomcat8/webapps/app.war"     
      sh "ssh ${user}@${ip} /opt/tomcat8/bin/shutdown.sh"
      sh "ssh ${user}@${ip} /opt/tomcat8/bin/startup.sh"
+     // ips.each{
+      //echo "Deploy on ${it}"
+      // sh "scp -o StrictHostKeyChecking=no target/*.war ${user}@${it}:/opt/tomcat8/webapps/app.war"     
+       //sh "ssh ${user}@${it} /opt/tomcat8/bin/shutdown.sh"
+       //sh "ssh ${user}@${it} /opt/tomcat8/bin/startup.sh"  
                     
-  }
+ // }
 }
